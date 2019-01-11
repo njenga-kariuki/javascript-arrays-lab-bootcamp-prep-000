fa1a2edfe629e5599da1994c4829911e5968197f
Njenga Kariuki <njengak@me.com>
2019-01-11 16:08:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,19 +19,25 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-   var kittens2 = [...kittens,name]
+   var kittens2 = Array.new
+   kittens2 = [...kittens,name]
+   return kittens2
 }
 
 function prependKitten(name) {
-   var kittens3 = [name,...kittens]
+   var kittens3 = Array.new
+   kittens3 = [name,...kittens]
+   return kittens3
 }
 
 function removeLastKitten() {
    var kittens4 = Array.new
    kittens4 = kittens.slice(0,kittens.length-1)
+   return kittens4
 }
 
 function removeFirstKitten() {
   var kittens5 = Array.new
   kittens5 = kittens.slice(1,kittens.length)
+  return kittens5
 }
