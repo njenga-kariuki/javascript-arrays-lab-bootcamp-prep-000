bae2689707e24c51844e0553392ac88f456636a2
Njenga Kariuki <njengak@me.com>
2019-01-11 16:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,34 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 
-function destructive
+function destructivelyAppendKitten(name) {
+   kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  kittens2 = [...kittens,name]
+}
+
+function prependKitten(name) {
+  kittens3 = [name,...kittens]
+}
+
+function removeLastKitten() {
+  kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten() {
+  kittens.slice(1,kittens.length)
+}
